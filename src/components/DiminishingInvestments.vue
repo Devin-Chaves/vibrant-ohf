2569aafcc8e4b8c811f70e7ac9b9d76ec9e9b422
Devin Chaves <dchav90@gmail.com>
2017-03-27 22:49:55 -0400

    ^_^

--- src/components/DiminishingInvestments.vue
+++ src/components/DiminishingInvestments.vue
@@ -5,7 +5,7 @@
       <div class="box box--wide">
         <div class="copy">
           <div class="big">Reality:</div>
-          <div>Seniors are the fastest growing economic sector, accounting for <br> $7.1 trillion in activity. And with global trends in life expectancy, this number is only expected to rise.</div>
+          <div>Seniors are the fastest growing economic sector, accounting for $7.1 trillion in activity. And with global trends in life expectancy, this number is only expected to rise.</div>
         </div>
       </div>
       <div class="box">
